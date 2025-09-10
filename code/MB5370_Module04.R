fbee5e0913fe66dc224d286feb7465949a480704
hrbfbi <heatherbrock57@gmail.com>
2025-09-10 10:53:42 +1000

    Add first script

---
+++ code/MB5370_Module04.R
@@ -0,0 +1,8 @@
+library(usethis)
+use_git_config(user.name = "hrbfbi", user.email = "heatherbrock57@gmail.com")
+gitcreds::gitcreds_set()
+gitcreds::gitcreds_get()
+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
+0
+1
+usethis::use_github()
